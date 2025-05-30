07fcd4392887bd27bc74c477211f761f6b9c5dca
Balaji <kanumillibalaji@gmail.com>
2025-05-30 20:56:59 +0530

    Developed a Bye feature

--- GitHubProject/src/com/Test.java
+++ GitHubProject/src/com/Test.java
@@ -9,4 +9,8 @@ public class Test {
 	public static void sayHello() {
 		System.out.println("Hi I am Developer 1");
 	}
+	
+	public static void sayBye() {
+		System.out.println("This is Developer 2");
+	}
 }
