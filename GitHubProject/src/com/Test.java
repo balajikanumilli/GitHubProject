c98e71815ec0652b78bcfcdfe5f20d1b7be24921
balajikanumilli <kanumillibalaji@gmail.com>
2025-05-31 03:55:38 +0530

    Update Test.java

--- GitHubProject/src/com/Test.java
+++ GitHubProject/src/com/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("I am a Manager...");
 		System.out.println("in master");
+		sayBye();
 	}
 	public static void sayHello() {
 		System.out.println("Hi I am Developer 1");
